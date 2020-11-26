3c0efca5c933ecdfd6d3f322c26a5fc2ccb877f0
Jasbir-Kaur481 <74880386+Jasbir-Kaur481@users.noreply.github.com>
2020-11-26 19:07:50 +1100

    Add files via upload

---
+++ LoopProgram/src/loopprogram/LoopProgram.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package loopprogram;
+
+import java.util.Scanner;
+
+
+public class LoopProgram {
+
+    
+    public static void main(String[] args) {
+        
+        for(int i=0; i< 5; i++){
+            
+        
+        
+       String name = "Jasbir";
+       
+       String address = "Sydney";
+       
+       String Subject = "Java";
+       
+       String Role = "Student";
+       
+       String Weather = "25  degree celcius";
+       
+       String date = "23.11.2020";
+       
+      
+        System.out.println("My name is:" + name);
+        System.out.println("My address is:" +address);
+        System.out.println("My subject is:" +Subject);
+        System.out.println("My role is:" +Role);
+        System.out.println("Today's weather is:" +Weather);
+        System.out.println("Today's date is:" +date);
+            System.out.println(".........................");
+        }
+        
+    }
+    
+}
